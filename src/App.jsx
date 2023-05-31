38131a65be0414d6b173b02ba32be42fc5741d31
Victor <chrollolucilfer1402@gmail.com>
2023-05-31 19:53:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app min-h-screen flex flex-col bg-drkBlue  justify-between text-white"></div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/*.{jsx, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", "monospace"],
+        sans: [
+          "Calibre",
+          "Inter",
+          "San Francisco",
+          "SF Pro Text",
+          "-apple-system",
+          "system-ui",
+          "sans-serif",
+        ],
+      },
+      colors: {
+        drkBlue: "#0A2942",
+        slate: "#636B78",
+        lightSlate: "#A1A6AD",
+        lightestSlate: "#D1D4D6",
+        bluishGrey: "#678398",
+        black: "#000000",
+        white: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
