f32d305675bbb63b04a56da125b35dc361fbbdae
Victor <chrollolucilfer1402@gmail.com>
2023-05-31 20:29:18 +0530

    fix issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Header } from "./header";
 
 function App() {
   return (
-    <div className="app min-h-screen flex flex-col bg-drkBlue  justify-between text-white"></div>
+    <div className="app min-h-screen max-w-md flex flex-col bg-drkBlue  justify-between text-white">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header>
+      <h1 className="mt-20 mb-5 text-5xl  text-center font-bold font-sans">
+        Checkout all the mini projects.
+      </h1>
+      <h5 className="subheading text-center text-lightSlate ">
+        This site contains all links to the github and live preview of all the
+        mini projects built with html css and javascript.
+      </h5>
+    </header>
+  );
+}
